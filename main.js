e2c9703e6523a65c9a61935890f14036a05875ee
Kairos Jeong <kairosys@gmail.com>
2022-04-10 20:41:13 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+//1  box 2
+//2. dropdown
+//3. currency inform
+//4. click : change item
+//5. exchange
+//6. 숫자를 한국어로
+//7. 밑에서 숫자를 바꿔도 위에서도
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("from-button").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches(".dropbtn")) {
+        var dropdown = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdown.length; i++) {
+            var openDropdown = dropdown[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+};
+
+let currencyRatio = {
+    USD: {
+        KRW: 1224.71,
+        USD: 1,
+        VND: 22864.5,
+        unit: "달러",
+    },
+    KRW: {
+        USD: 0.00082,
+        KRW: 1,
+        VND: 18.67,
+        unit: "원",
+    },
+    VND: {
+        CND: 1,
+        USD: 0.000044,
+        KRW: 0.054,
+        unit: "동",
+    },
+};
+
+//console.log(currencyRatio.USD.unit);
+//console.log(currencyRatio["KRW"]["unit"])
+let fromCurrency = "USD";
+document.querySelectorAll("#from-currency-list a").forEach((menu) =>
+    menu.addEventListener("click", function () {
+        //1.버튼을 가져온다
+        //2.버튼에 값을 바꾼다
+        //3.선택된 currency값을 변수에 저장해준다
+        document.getElementById("from-button").textContent = this.textContent;
+        fromCurrency = this.textContent;
+        console.log("from crurrency is" + fromCurrency);
+    })
+);
+
+let toCurrency = "USD"
+document
+    .querySelectorAll("#to-currency-list a")
+    .forEach((menu) => menu.addEventListener("click", function () {
+        document.getElementById("to-button").textContent = this.textContent;
+        toCurrency = this.textContent;
+    }));
